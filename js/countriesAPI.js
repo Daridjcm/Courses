d4964f59e1473b168fb5f5d12c048e7e66b8eb2d
Daridjcm <daricorreamedina@gmail.com>
2024-07-19 16:40:35 -0500

    Country validation checked

--- js/countriesAPI.js
+++ js/countriesAPI.js
@@ -2,23 +2,27 @@ let dataResArr;
 const dataResObj = {};
 
 const uploadDatas = async() => {
-    const res = await fetch('https://restcountries.com/v3.1/lang/spanish')
+    const res = await fetch('https://restcountries.com/v3.1/lang/spanish');
     let datas = await res.json();
     datas = datas.map(data => data.name.common);
     dataResArr = [...datas];
     let convertArrToObj = () => {
-        for (let i = 1; i < dataResArr.length; i++) {
+        for (let i = 0; i < dataResArr.length; i++) {
             dataResObj[i] = dataResArr[i];
-        };
+        }
     };
     convertArrToObj();
     validateCountrie();
 }
 
 const validateCountrie = () => {
-// Validación de paises, se agregará emojis al lado del valor del objeto
-    // dataResObj === 'Colombia' ? dataResObj.join('🇨🇴') : undefined
-    console.log(dataResObj)
+// Country validation, emoji will be added next to the object value    
+    for (let key in dataResObj) {
+        if (dataResObj[key] === 'Colombia') {
+            dataResObj[key] += ' 🇨🇴';
+        }
+    }
+    console.log(dataResObj);
 }
 
 uploadDatas();
